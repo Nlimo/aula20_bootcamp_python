7c3750af411a1247b11852a289c720fb37d30a89
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-18 12:55:43 -0300

    add class ProductReponse

--- backend/schema.py
+++ backend/schema.py
@@ -3,7 +3,7 @@ from enum import Enum
 from datetime import datetime
 from typing import Optional
 
-# A função do schema é validar como os dados vão trafegar, por isso usamos o pydantic
+# A função do schema é validar como os dados vão trafegar, ou seja que o usuário estão inserindo/atualizando, por isso usamos o pydantic
 
 class ProductBase(BaseModel):
     name: str
@@ -15,3 +15,12 @@ class ProductBase(BaseModel):
 class ProductCreat(ProductBase):
     pass
 
+# O único que vai conversar com meu models, pois é para fazermos um select no banco, então queremos ver todos os dados, diferente do usuário que não precisa criar id nem a data de criação do produto
+class ProductReponse(ProductBase):
+    id: int
+    created_at: datetime
+
+    class Config:
+        from_attributes: True
+
+class             

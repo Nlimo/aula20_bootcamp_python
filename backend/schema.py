3c92ec773cb4d166cecf08fa34ec6c93fd1651c0
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-18 12:50:39 -0300

    add class ProductCreat

--- backend/schema.py
+++ backend/schema.py
@@ -3,6 +3,8 @@ from enum import Enum
 from datetime import datetime
 from typing import Optional
 
+# A função do schema é validar como os dados vão trafegar, por isso usamos o pydantic
+
 class ProductBase(BaseModel):
     name: str
     description: str
@@ -10,5 +12,6 @@ class ProductBase(BaseModel):
     categoria: str
     email_fornecedor: EmailStr
 
-
+class ProductCreat(ProductBase):
+    pass
 

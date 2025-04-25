068c300f8dd96c1c421be5eca3f686eeef526015
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-04-25 17:19:06 -0300

    creat functions get_products, return table products all and get_product, return table products filtered

--- backend/crud.py
+++ backend/crud.py
@@ -2,3 +2,19 @@ from sqlalchemy.orm import Session
 from schemas import ProductBase, ProductCreate
 from models import ProductModel
 
+# get all (select * from)
+
+def get_products(db: Session):
+    "Função onde retorna todos os produtos"
+    return db.query(ProductModel).all()
+
+# get where id = 1
+def get_product(db: Session, product_id: int):
+    "Função onde retorna somente um produto, usando where"
+    return db.query(ProductModel).filter(ProductModel.id == product_id).first()
+
+# insert into (creat)
+
+# update where id = 1
+
+# delete where id = 1

18343fc83af414a5599dd78569e6f5c7d1efc7e3
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-05-06 19:22:22 -0300

    att code of router patch

--- backend/router.py
+++ backend/router.py
@@ -44,8 +44,8 @@ def delete_product_route(product_id: int, db: Session = Depends(get_db)):
 
 ### criar minha rota de fazer um update nos itens
 @router.patch("/products/{produto}}", response_model=ProductReponse)
-def att_product(product_id: int, product: ProductUpdate,db: Session = Depends(get_db)):
-    db_product = update_product(db=db, product_id=product_id, product=product)
+def update_product_route(product_id: int, product: ProductUpdate,db: Session = Depends(get_db)):
+    db_product = update_product(db=db, product_id=product_id)
     if db_product is None:
         raise HTTPException(status_code=404, detail="o id do produto não existe")
     return delete_product(db_product=db_product, product_id=product_id, db=db)

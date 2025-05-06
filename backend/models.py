56db17433446093faf4a2df8a50d5aeb54809b4f
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-05-06 19:49:56 -0300

    correting code of importing in models

--- backend/models.py
+++ backend/models.py
@@ -1,6 +1,6 @@
 from sqlalchemy import Column, Integer, String, Float, DateTime
 from sqlalchemy.sql import func
-from database import Base
+from backend.database import Base
 
 class ProductModel(Base):
     __tablename__ = "products"  # esse será o nome da tabela

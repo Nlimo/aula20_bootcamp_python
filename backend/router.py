c59e8c602684c15675d6204042ba8ad6a852b90e
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-05-06 19:39:56 -0300

    add att in codes routes of produto for product

--- backend/router.py
+++ backend/router.py
@@ -21,7 +21,7 @@ def read_all_products(db: Session = Depends(get_db)):
     return products
 
 ### criar minha rota de buscar 1 item
-@router.get("/products/{produto}", response_model=ProductReponse)
+@router.get("/products/{product}", response_model=ProductReponse)
 def read_one_peroduct(product_id:int, db:Session = Depends(get_db)):
     db_product = get_product(db=db, product_id=product_id)
     if db_product is None:
@@ -29,12 +29,12 @@ def read_one_peroduct(product_id:int, db:Session = Depends(get_db)):
     return db_product
 
 ### criar minha rota de add 1 item
-@router.post("/products/{produto}", response_model=ProductReponse)
+@router.post("/products/{product}", response_model=ProductReponse)
 def create_product_route(product: ProductCreate, db:Session = Depends(get_db)):
     return create_product(product=product,db=db)
 
 ### criar minha rota de deletar um item
-@router.delete("/products/{produto}}", response_model=ProductReponse)
+@router.delete("/products/{product}}", response_model=ProductReponse)
 def delete_product_route(product_id: int, db: Session = Depends(get_db)):
     db_product = delete_product(product_id=product_id, db=db)
     if db_product is None:
@@ -45,7 +45,7 @@ def delete_product_route(product_id: int, db: Session = Depends(get_db)):
 ### criar minha rota de fazer um update nos itens
 @router.patch("/products/{product_id}}", response_model=ProductReponse)
 def update_product_route(product_id: int, product: ProductUpdate,db: Session = Depends(get_db)):
-    db_product = update_product(db=db, product_id=product_id)
+    db_product = update_product(db=db, product_id=product_id, product=product)
     if db_product is None:
         raise HTTPException(status_code=404, detail="o id do produto não existe")
     return db_product

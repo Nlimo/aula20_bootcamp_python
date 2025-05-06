56378c49a884e21cf5cd4c1c23a582ef15e093dc
Lucas William da Silva Chaves <lucaswilliamsv@yahoo.com.br>
2025-05-06 17:25:08 -0300

    add code of the router get products

--- backend/router.py
+++ backend/router.py
@@ -13,12 +13,21 @@ from crud import (
 
 router = APIRouter()
 
-### criar minha rota de buscar 1 item
-
 ### criar minha rota de buscar todos os itens
+### sempre vamos ter 2 atributos obrigatórios, o PATH (endereço da minha api)e o RESPONSE)
+@router.get("/products/", response_model=List[ProductReponse])
+def read_all_products(db: Session = Depends(get_db)):
+    products = get_products(db)
+    return products
+
+### criar minha rota de buscar 1 item
+@router.get()
 
 ### criar minha rota de add 1 item
+@router.post()
 
 ### criar minha rota de deletar um item
+@router.delete()
 
-### criar minha rota de fazer um update nos itens   
+### criar minha rota de fazer um update nos itens
+@router.update()
